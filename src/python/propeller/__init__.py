c878c422c86657171bbb1b08fd9693eb7c4b8666
Christian Wengert <christian.wengert@smartpowerpool.ch>
2018-09-05 11:14:12 +0200

    Full motor control API

--- src/python/propeller/__init__.py
+++ src/python/propeller/__init__.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-import http
 import struct
 from time import sleep
 from typing import Tuple
@@ -7,12 +6,13 @@ import requests
 import sh
 
 
+FULL_TURN = 360.0
+
+PHI_GEAR_RATIO = 100.0 / 2684.0
+
+
 ZAXIS = "192.168.1.222"
 PHI = "192.168.1.102"
-
-
-
-GEARS = {ZAXIS: (1,1), PHI: (100, 2684)}
 
 
 def read_data(ip: str) -> Tuple[int, int, int, int]:
@@ -23,87 +23,84 @@ def read_data(ip: str) -> Tuple[int, int, int, int]:
     return uptime, position, speed, current
 
 
-def read_pos(ip: str) -> float:
+def read_angular_pos(ip: str) -> float:
     uptime, position, speed, current = read_data(ip)
     return position / 10.0
 
 
-def control_pos(pos: int, speed: int=10000, current: int=20000, acc: int=1000, decc: int=1000):
+def set_angular_pos(pos: int, speed: int=10000, current: int=20000, acc: int=1000, decc: int=1000):
     return f'<control pos="{pos}" speed="{speed}" current="{current}" mode="129" acc="{acc}" decc="{decc}" />'
 
 
 def send_pos(ip: str, position: float):
-
     headers = {'type': 'text/plain'}
     position = int(position * 10.0)
-    data = control_pos(0)
-    r = requests.post(f'http://{ip}/writeTicket.cgi',
-                      headers=headers,
-                      data=control_pos(position))
+    requests.post(f'http://{ip}/writeTicket.cgi',
+                  headers=headers,
+                  data=set_angular_pos(position))
 
+
+def move_phi(pos: float):
+    """
+    Give the angular position of the output shaft in [deg]
+    """
+
+    motor_pos = pos / PHI_GEAR_RATIO
+
+    send_pos(PHI, motor_pos)
+
+
+def read_phi() -> float:
+    """
+    Returns the output shafts position in [deg]
+    """
+
+    motor_pos = read_angular_pos(ZAXIS)
+    pos = motor_pos * PHI_GEAR_RATIO
+    return pos
+
+
+def move_z(pos: float):
+    """
+    pos in [mm]
+    the openbuilds lead screw has a pitch of 2mm
+
+    360.0deg == 2mm
+    1deg == 2/360 = 0.005555..
+
+    Accuracy is theoretically around 2.5um
+
+    """
+    pitch = 8.0  # [mm]
+    angular_pos = pos / pitch * 360.0
+    send_pos(ZAXIS, angular_pos)
+
+
+def read_z() -> float:
+    """
+    Return pos in [mm]]
+
+    Accuracy is theoretically around 2.5um
+    """
+    pitch = 8.0  # [mm]
+    angular_pos = read_angular_pos(ZAXIS)
+    pos = angular_pos * pitch / FULL_TURN
+    return pos
 
 
 def main():
     #
-    send_pos(ZAXIS, 0)
-    print(read_pos(ZAXIS))
+    move_z(0)
+    print(read_z())
     sleep(5)
-    send_pos(ZAXIS, 360)
-    print(read_pos(ZAXIS))
+    move_z(10)
+    print(read_z())
     sleep(5)
-    send_pos(ZAXIS, 3600)
-    print(read_pos(ZAXIS))
+    move_z(100)
+    print(read_z())
     sleep(5)
-    print(read_pos(ZAXIS))
-    #                  http://192.168.178.222/getData.cgi?bin
-
-    # try:
-    #     r = requests.get(f'http://{ZAXIS}/getData.cgi?bin')
-    # except Exception as e:
-    #     x = e
-    # print(x)
-
-    # r = requests.get(f'http://{ZAXIS}/getData.cgi?bin', headers=HEADERS)
-    # a = struct.unpack('iiii', r._content[0:16])
-    # uptime, position, speed, current = a
-    # print(a)
-    #
-    # r = requests.post(f'http://{ZAXIS}/writeTicket.cgi', headers=HEADERS, data=control_pos(1000))
-    # r = requests.post(f'http://{ZAXIS}/writeTicket.cgi', headers=HEADERS, data=control_pos(-2000))
-    # r = requests.post(f'http://{ZAXIS}/writeTicket.cgi', headers=HEADERS, data=control_pos(5000))
-    # r = requests.post(f'http://{ZAXIS}/writeTicket.cgi', headers=HEADERS, data=control_pos(-10000))
-    # r = requests.post(f'http://{ZAXIS}/writeTicket.cgi', headers=HEADERS, data=control_pos(10000))
-    # r = requests.post(f'http://{ZAXIS}/writeTicket.cgi', headers=HEADERS, data=control_pos(0))
-    # #
+    print(read_z())
 
 
 if __name__ == "__main__":
     main()
-# curl 'http://192.168.178.222/getData.cgi?bin' -H 'Pragma: no-cache' -H 'DNT: 1' -H
-# 'Accept-Encoding: gzip, deflate' -H
-# 'Accept-Language: en-GB,en-US;q=0.9,en;q=0.8' -H
-# 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 ' \
-# '(KHTML, like Gecko) Chrome/65.0.3325.183 Safari/537.36 Vivaldi/1.96.1147.64'
-#
-# -H 'Accept: */*' -H 'Referer: http://192.168.178.222/'
-# -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' --compressed
-
-#
-#
-# headers = {
-# 'Host': '192.168.1.102',
-# 'Connection': 'keep-alive',
-# 'Pragma': 'no-cache',
-# 'Cache-Control': 'no-cache',
-# 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.183 Safari/537.36 Vivaldi/1.96.1147.64',
-# 'Accept': '*/*',
-# 'DNT': '1',
-# 'Referer': 'http://192.168.1.102/',
-# 'Accept-Encoding': 'gzip, deflate',
-# 'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
-# }
-#
-#
-#
-# h1 = http.client.HTTPConnection(ZAXIS)
-# h1.request('GET', '/getData.cgi?bin', body=None, headers={}, encode_chunked=False)

36d10c89dc7b0a5a2f90d56919d517c5bf1c141f
Christian Wengert <christian.wengert@smartpowerpool.ch>
2018-11-22 11:12:43 +0100

    Z=Axis Error handling

--- src/python/propeller/__init__.py
+++ src/python/propeller/__init__.py
@@ -2,8 +2,6 @@
 import socket
 # import struct
 from time import sleep
-# from typing import Tuple
-# import sh
 
 POS_CONTROL = 129
 
@@ -15,24 +13,9 @@ PITCH = 8
 FULL_TURN = 360.0
 MULTIPLIER = 10.0
 
-# PHI_GEAR_RATIO = 100.0 / 2684.0
-
 
 ZAXIS = "192.168.178.11"
 PHI = "192.168.178.12"
-
-
-# def read_data(ip: str) -> Tuple[int, int, int, int]:
-#     noinspection PyUnresolvedReferences
-    # r = sh.curl(f'http://{ip}/getData.cgi?bin')
-    # data = r.stdout
-    # uptime, position, speed, current = struct.unpack('iiii', data[0:16])
-    # return uptime, position, speed, current
-#
-#
-# def read_angular_pos(ip: str) -> float:
-#     uptime, position, speed, current = read_data(ip)
-#     return position / MULTIPLIER
 
 
 def control_ticket(mode: int, pos: int, speed: int=10000, current: int=20000, acc: int=400, decc: int=400):
@@ -96,24 +79,40 @@ def parse(msg):
     return pos, speed, torque, uptime
 
 
+def sendall(socket, data):
+    # try:
+    #     read_socket(socket)
+    # except Exception as e:
+    #     return False
+
+    # try:
+    socket.sendall(data)
+    # except Exception as e:
+    #     return False
+    # return True
+
+
 def main():
     z_socket = connect(ZAXIS)
     phi_socket = connect(PHI)
 
     def stop_all():
-        z_socket.sendall(control_ticket(mode=0, speed=0, current=0, pos=0).encode())
-        phi_socket.sendall(control_ticket(mode=0, speed=0, current=0, pos=0).encode())
+        sendall(z_socket, control_ticket(mode=0, speed=0, current=0, pos=0).encode())
+        # sendall(phi_socket, control_ticket(mode=0, speed=0, current=0, pos=0).encode())
 
     z_err = 0.0
     phi_err = 0.0
 
     total_length = 4 * l0 + 2 * l1
-    z_socket.sendall(control_ticket(mode=POS_CONTROL, speed=SPEED, current=CURRENT, pos=0).encode())
-    phi_socket.sendall(control_ticket(mode=POS_CONTROL, speed=SPEED, current=CURRENT, pos=0).encode())
-    sleep(10)
+    sendall(z_socket, control_ticket(mode=POS_CONTROL, speed=SPEED, current=CURRENT, pos=0).encode())
+    # sendall(phi_socket, control_ticket(mode=POS_CONTROL, speed=SPEED, current=CURRENT, pos=0).encode())
+    while abs(read_motor_position(z_socket)) > 0.5:
+        sleep(0.1)
 
-    STEP = 20
+    STEP = 100  # = 2.0deg
     z_target = 0
+    angular_z = 0
+    angular_phi = 0
 
     reinit = False
     i = 1
@@ -122,48 +121,41 @@ def main():
         # noinspection PyBroadException
         try:
             if reinit:
-                try:
-                    # get current position positions
-                    current_z = read_angular_position(z_socket)
-                    angular_phi = read_angular_position(phi_socket)
-                    # increment errors
-                    z_err = z_err + current_z
-                    phi_err = phi_err + angular_phi
 
-                    # ensure we are @ zero!
-                    z_socket.sendall(system_ticket(mode=2).encode())
+                # reset_motors(phi_socket, z_socket)
+                current_z = read_motor_position(z_socket)
+                # increment errors
+                z_err += angular_z - current_z
+                # phi_err = phi_err + angular_phi
 
-                    phi_socket.sendall(system_ticket(mode=2).encode())
+                # reset target
+                z_target = current_z
 
-                    # reset target
-                    z_target = 0
+                reinit = False
 
-                    reinit = False
-                except Exception as e:
-                    continue
-
-            z_socket.sendall(control_ticket(mode=133, speed=SPEED, current=CURRENT, pos=z_target).encode())
+            sendall(z_socket, control_ticket(mode=133, speed=SPEED, current=CURRENT, pos=z_target).encode())
             z_target += STEP
 
-            angular_z = read_angular_position(z_socket)
+            angular_z = read_motor_position(z_socket)
 
             real_z = (angular_z + z_err) / MULTIPLIER / FULL_TURN * PITCH
 
-            angular_phi = read_angular_position(phi_socket)
-
-            phi_target = int(f(real_z) * MULTIPLIER) - phi_err
-
-            if abs(phi_target - angular_phi) > 0.1:
-                phi_socket.sendall(control_ticket(mode=133, speed=SPEED, current=CURRENT, pos=phi_target).encode())
+            # angular_phi = read_motor_position(phi_socket)
+            #
+            # phi_target = int(f(real_z) * MULTIPLIER) - phi_err
+            #
+            # if abs(phi_target - angular_phi) > 0.1:
+            #     sendall(phi_socket, control_ticket(mode=133, speed=SPEED, current=CURRENT, pos=phi_target).encode())
 
             if real_z >= total_length:
                 stop_all()
                 break
 
+            phi_target = -9999
             print(real_z, phi_target / MULTIPLIER, (angular_phi + phi_err) / MULTIPLIER)
             i += 1
             sleep(1.0 / 10.0)
-        #
+
         except Exception as e:
 
             # first stop all;
@@ -180,9 +172,20 @@ def main():
             reinit = True
 
 
-def read_angular_position(socket):
+def reset_motors(phi_socket, z_socket):
+    # ensure we are @ zero!
+    sendall(z_socket, system_ticket(mode=2).encode())
+    sleep(0.5)
+    sendall(z_socket, system_ticket(mode=2).encode())
+    # sendall(phi_socket, system_ticket(mode=2).encode())
+    # sleep(0.5)
+    # sendall(phi_socket, system_ticket(mode=2).encode())
+
+
+def read_motor_position(socket):
     args = read_socket(socket)
     return args[0]
+
 
 def read_socket(socket):
     latest = socket.recv(1024 * 1024)[-85 * 2:].decode()
